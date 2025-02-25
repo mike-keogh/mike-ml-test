5f810a31fcffa03e3a1b507c69dfea30c63bf60f
Mike <mike.brooke.keogh@gmail.com>
2025-02-25 18:02:03 +1300

    pull data from rudimentary backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 import { Navigation } from "./components/Navigation";
@@ -6,12 +7,18 @@ import { fetchData } from "./data";
 function App() {
   const [data, setData] = useState<any>();
   useEffect(() => {
-    fetchData()
-      .then((res) => setData(res))
-      .catch((error) => {
-        console.error("there was an error fetching data", error);
-        window.location.reload();
+    if (import.meta.env.DEV) {
+      axios.get("http://localhost:3000/data").then((res) => {
+        setData(res.data.backendData);
       });
+    } else {
+      fetchData()
+        .then((res) => setData(res))
+        .catch((error) => {
+          console.error("there was an error fetching data", error);
+          window.location.reload();
+        });
+    }
   }, []);
 
   return (
